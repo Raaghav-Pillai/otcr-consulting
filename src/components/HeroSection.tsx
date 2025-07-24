f160b68d00e1ee6aaf6e90b176b4816f5b912354
Raaghav-Pillai <raaghavpillai@gmail.com>
2025-07-24 16:04:59 +0530

    added react bits

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { ArrowDown } from 'lucide-react';
+import SplitText from '../reactbits/textanimations/SplitText/SplitText';
 
 const HeroSection = () => {
   return (
@@ -6,11 +7,10 @@ const HeroSection = () => {
       <div className="flex-1 flex flex-col justify-center items-center w-full">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-4xl md:text-6xl lg:text-6xl font-extrabold text-white leading-tight mb-4">
-            The Premier Student-Run<br />
-            Consulting Firm
+            OTCR
           </h1>
           <h2 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-accent mb-10">
-            at the University of Illinois
+            Consulting
           </h2>
         </div>
       </div>
